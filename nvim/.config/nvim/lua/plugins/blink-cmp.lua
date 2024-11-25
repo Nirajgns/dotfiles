625920a7acf7104ba30ac75a560b691b0c567d12
nirajgns <nirajgns@gmail.com>
2024-11-25 14:38:17 +0545

    blink config

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,5 +1,6 @@
 return {
   "saghen/blink.cmp",
+  -- enabled = false,
   event = "InsertEnter",
   version = "v0.*",
   opts_extend = {
@@ -18,21 +19,23 @@ return {
       windows = {
         autocomplete = {
           border = "rounded",
-          winblend = 0,
+          winblend = 20,
           selection = "preselect",
-          winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+          winhighlight = "Normal:CatppuccinSurface0,FloatBorder:CatppuccinSurface2,Search:None",
         },
         documentation = {
           auto_show = true,
           border = "rounded",
-          winblend = 0,
+          winblend = 20,
           min_width = 1,
           min_height = 1,
+          winhighlight = "Normal:CatppuccinSurface0,FloatBorder:CatppuccinSurface2,Search:None",
         },
         signature_help = {
           auto_show = true,
           border = "rounded",
-          winblend = 0,
+          winhighlight = "Normal:CatppuccinSurface0,FloatBorder:CatppuccinSurface2,Search:None",
+          winblend = 20,
         },
         ghost_text = {
           enabled = false,
