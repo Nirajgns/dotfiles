17331a75802c2e6193d3dd812f3321ff8dc16219
nirajgns <nirajgns@gmail.com>
2025-01-31 18:41:47 +0545

    configured copilot autocompletion

--- nvim/.config/nvim/lua/plugins/copilotchat.lua
+++ nvim/.config/nvim/lua/plugins/copilotchat.lua
@@ -11,9 +11,68 @@ return {
       { mode = { "v" }, "<leader>aa", ":CopilotChat<cr>", desc = "Copilot chat toggle", silent = true },
       { mode = { "v", "n" }, "<leader>ao", ":CopilotChatOptimize<cr>", desc = "Copilot optimize", silent = true },
       { mode = { "v", "n" }, "<leader>at", ":CopilotChatTests<cr>", desc = "Copilot generate tests", silent = true },
+      {
+        "<leader>ap",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.snacks").pick(actions.prompt_actions(), {
+            layout = "select",
+          })
+        end,
+        desc = "CopilotChat - Prompt actions",
+      },
     },
     dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      -- { "github/copilot.vim" },
+      {
+        "zbirenbaum/copilot.lua",
+        config = function()
+          require("copilot").setup({
+            panel = {
+              enabled = false,
+              auto_refresh = false,
+              keymap = {
+                jump_prev = "[[",
+                jump_next = "]]",
+                accept = "<CR>",
+                refresh = "gr",
+                open = "<M-CR>",
+              },
+              layout = {
+                position = "bottom", -- | top | left | right | horizontal | vertical
+                ratio = 0.4,
+              },
+            },
+            suggestion = {
+              enabled = true,
+              auto_trigger = true,
+              hide_during_completion = false,
+              debounce = 100,
+              keymap = {
+                accept = "<A-f>",
+                accept_word = "<A-w>",
+                accept_line = "<A-l>",
+                next = "<A-n>",
+                prev = "<A-p>",
+                dismiss = "<A-c>",
+              },
+            },
+            filetypes = {
+              yaml = false,
+              markdown = false,
+              help = false,
+              gitcommit = false,
+              gitrebase = false,
+              hgcommit = false,
+              svn = false,
+              cvs = false,
+              ["."] = false,
+            },
+            copilot_node_command = "node", -- Node.js version must be > 18.x
+            server_opts_overrides = {},
+          })
+        end,
+      },
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     config = function()
