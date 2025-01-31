1bc262e3632e963360b2f8748f3cd8706b841b1c
nirajgns <nirajgns@gmail.com>
2025-01-31 21:16:47 +0545

    minor refactoring

---
+++ nvim/.config/nvim/lua/plugins/utils/copilot.lua
@@ -0,0 +1,50 @@
+return {
+  "zbirenbaum/copilot.lua",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = false,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+        layout = {
+          position = "bottom", -- | top | left | right | horizontal | vertical
+          ratio = 0.4,
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = false,
+        debounce = 100,
+        keymap = {
+          accept = "<A-f>",
+          accept_word = "<A-w>",
+          accept_line = "<A-l>",
+          next = "<A-n>",
+          prev = "<A-p>",
+          dismiss = "<A-c>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = "node", -- Node.js version must be > 18.x
+      server_opts_overrides = {},
+    })
+  end,
+}
--- nvim/.config/nvim/lua/plugins/utils/copilotchat.lua
+++ nvim/.config/nvim/lua/plugins/utils/copilotchat.lua
@@ -23,63 +23,16 @@ return {
       },
     },
     dependencies = {
-      -- { "github/copilot.vim" },
-      {
-        "zbirenbaum/copilot.lua",
-        config = function()
-          require("copilot").setup({
-            panel = {
-              enabled = false,
-              auto_refresh = false,
-              keymap = {
-                jump_prev = "[[",
-                jump_next = "]]",
-                accept = "<CR>",
-                refresh = "gr",
-                open = "<M-CR>",
-              },
-              layout = {
-                position = "bottom", -- | top | left | right | horizontal | vertical
-                ratio = 0.4,
-              },
-            },
-            suggestion = {
-              enabled = true,
-              auto_trigger = true,
-              hide_during_completion = false,
-              debounce = 100,
-              keymap = {
-                accept = "<A-f>",
-                accept_word = "<A-w>",
-                accept_line = "<A-l>",
-                next = "<A-n>",
-                prev = "<A-p>",
-                dismiss = "<A-c>",
-              },
-            },
-            filetypes = {
-              yaml = false,
-              markdown = false,
-              help = false,
-              gitcommit = false,
-              gitrebase = false,
-              hgcommit = false,
-              svn = false,
-              cvs = false,
-              ["."] = false,
-            },
-            copilot_node_command = "node", -- Node.js version must be > 18.x
-            server_opts_overrides = {},
-          })
-        end,
-      },
+      { "zbirenbaum/copilot.lua" },
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     config = function()
       require("CopilotChat").setup({
+        question_header = "##    USER",
+        answer_header = "##     COPILOT",
+        error_header = "##     ERROR",
         chat_autocomplete = true,
-
-        separator = "~~~~~~~", -- Separator to use in chat
+        separator = "", -- Separator to use in chat
         window = {
           -- layout = "float",
           -- relative = "cursor",
@@ -100,8 +53,19 @@ return {
           },
         },
         mappings = {
+          close = {
+            normal = "q",
+          },
           complete = {
             insert = "A-f",
+          },
+          submit_prompt = {
+            normal = "<CR>",
+            insert = "<CR>",
+          },
+          reset = {
+            normal = "<C-c>",
+            insert = "<C-c>",
           },
         },
       })
